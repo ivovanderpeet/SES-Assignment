0a203cb0786a82c104a858ed7dd550e6ce488e8f
Jorrit Rijk <j.j.a.rijk@student.tue.nl>
2022-01-16 11:57:52 +0100

    2.2 met condensor HX

--- A_2_2.m
+++ A_2_2.m
@@ -117,31 +117,99 @@ Wecon = getToutCp(Wecon);
 Rcond.Tin = T(5);
 Rcond.Tout = T(1);
 Rcond.Tm = (Rcond.Tin+Rcond.Tout)/2;
+Rcond.p = p(1);
 
 DHcond.Tout = 110;
 DHcond.V = 150e-3;  % [m3/s]
 DHcond.p = 1.53e5;
 DHcond = getToutCp(DHcond);
 
+%% Condensor heat exchanger
+
+% Determine dTlm and UA
+dT1 = Rcond.Tout - DHcond.Tin;
+dT2 = Rcond.Tin - DHcond.Tout;
+dTlm = (dT2 - dT1)./log(dT2./dT1);
+UA = -Rcond.Qdot./dTlm; %Rcond.Qdot = DHcond.Qdot
+
+% Determine mean density
+DHcond.rho = XSteam('rho_pT', DHcond.p/1e5, DHcond.Tm);
+
+% Determine average dynamic viscosity
+DHcond.mu = XSteam('my_pT', DHcond.p/1e5, DHcond.Tm);
+
+% Determine thermal conductivity
+DHcond.k = XSteam('tc_pT', DHcond.p/1e5, DHcond.Tm);
+
+%Determine heat capacity
+DHcond.Cp = XSteam('Cp_pT', DHcond.p/1e5, DHcond.Tm)*1000;
+
+% Determine Prandtl number
+DHcond.Pr = DHcond.Cp*DHcond.mu/DHcond.k;
+
+% Volume flow
+% Rcond.V = Rcond.mdot/Rcond.rho;
+Rcond.V = nan;
+DHcond.V = DHcond.mdot/DHcond.rho;
+
+%HX
+Rcond.HX.L = 3.7;
+DHcond.HX.L = Rcond.HX.L;
+Rcond.HX.D = 20e-3;
+DHcond.HX.D = Rcond.HX.D;
+
+Rcond.HX.Nplatey = 15;
+DHcond.HX.Nplatey = Rcond.HX.Nplatey;
+Rcond.HX.Nplatex = 15;
+DHcond.HX.Nplatex = Rcond.HX.Nplatex;
+
+CD.W = Rcond.HX.Nplatex*Rcond.HX.D*2;
+CD.H = Rcond.HX.Nplatey*Rcond.HX.D*2;
+CD.L = Rcond.HX.L;
+
+Rcond.HX.rough = 0.015e-3;
+DHcond.HX.rough = Rcond.HX.rough;
+
+[Cond.R,Cond.DH] = HX(Cond.R,Cond.DH);
+
+% Heat transfer coef.
+CD.t = 1e-3;
+CD.kwall = 50; % STEEL
+CD.U = (CD.t/CD.kwall + 1/DHcond.HX.h)^-1;
+CD.A = UA/CD.U;
+
 %% Figure
 sat = getSatCurve();
 
-% figure(1)
-% plot([s,s(1)],[T,T(1)]); hold on
-% plot(sat.s,sat.T)
-% title('T-s diagram')
-% xlabel('Entropy s [kJ/kg/K]')
-% ylabel('Temperature [C]')
-% text(s,T,{'1','2','3','4','5'})
-% grid on
+figure(1)
+plot([s,s(1)],[T,T(1)]); hold on
+plot(sat.s,sat.T)
+title('T-s diagram')
+xlabel('Entropy s [kJ/kg/K]')
+ylabel('Temperature [C]')
+text(s,T,{'1','2','3','4','5'})
+grid on
 
-% figure(2)
-% plot([h,h(1)],[T,T(1)]); hold on
-% plot(sat.h,sat.T);
-% plot([h(1),h(5)],[DHcond.Tin,DHcond.Tout]);
-% plot([h(2),h(3),h(4)],[Wecon.Tout,Wecon.Tin,Wevap.Tin]);
-% title('Pinch diagram')
-% xlabel('Enthalpy h [J/kg]')
-% ylabel('Temperature [C]')
-% text(h,T,{'1','2','3','4','5'})
-% grid on
+figure(2)
+plot([h,h(1)],[T,T(1)]); hold on
+plot(sat.h,sat.T);
+plot([h(1),h(5)],[DHcond.Tin,DHcond.Tout]);
+plot([h(2),h(3),h(4)],[Wecon.Tout,Wecon.Tin,Wevap.Tin]);
+title('Pinch diagram')
+xlabel('Enthalpy h [J/kg]')
+ylabel('Temperature [C]')
+text(h,T,{'1','2','3','4','5'})
+grid on
+
+fprintf('CONDENSOR HEAT EXCHANGER PROPERTIES\n')
+fprintf('Heat-trns coef:\tU = %0.2f\t\t[W/m2/K]\n',CD.U)
+fprintf('Reqired area:\tA = %0.2f\t\t[m2]\n',CD.A)
+fprintf('Total length:\tL = %0.2f\t\t[m]\n',CD.L)
+fprintf('Total width:\tW = %0.2f\t\t[m]\n',CD.W)
+fprintf('Total height:\tH = %0.2f\t\t[m]\n',CD.H)
+fprintf('Pres. drop:\t\tdp = %0.2e\tdp = %0.2e\t[Pa]\n', Rcond.HX.dp, DHcond.HX.dp)
+fprintf('Conv. coef.:\th = %0.2e\th = %0.2e\t[W/m2/K]\n', Rcond.HX.h, DHcond.HX.h)
+fprintf('Nusselt number:\tNu = %0.1f\t\tNu = %0.1f\t\t[-]\n', Rcond.HX.Nu, DHcond.HX.Nu)
+fprintf('HX Area:\t\tA = %0.2f\t\tA = %0.2f\t\t[m2]\n', Rcond.HX.Aht, DHcond.HX.Aht)
+fprintf('Velocity:\t\tv = %0.2f\t\tv = %0.2f\t\t[m/s]\n', Rcond.HX.v, DHcond.HX.v)
+fprintf('Reynolds:\t\tRe = %0.3e\tRe = %0.3e\t[-]\n\n', Rcond.HX.Re, DHcond.HX.Re)
